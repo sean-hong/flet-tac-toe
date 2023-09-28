cb6d68dcd986869c36a483e01a7aab93efc1acb5
Sean Hong <hongsean1@gmail.com>
2023-09-27 22:57:56 -0400

    added cell borders
    used an image to do so

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ def main(page: ft.Page):
 
     # click event
     def do_something(e):
-        e.control.content.value = 'x'
+        e.control.content.controls[1].content.value = 'x'
         display_symbols[e.control.key] = 'x'
         page.update()
 
@@ -20,13 +20,23 @@ def main(page: ft.Page):
         grid_board.controls.append(
             ft.GestureDetector(
                 key = i,
-                content = ft.Text(
-                    value = '',
-                    expand = True,
-                    text_align = ft.TextAlign.CENTER,
-                    color = "white",
-                    size = 50,
-                    bgcolor = "blue"
+                content = ft.Stack(
+                    [
+                        ft.Image(
+                            "https://www.svgrepo.com/show/101416/square-outline.svg",
+                            expand=True
+                        ),
+                        ft.Container(
+                            content = ft.Text(
+                                value = '',
+                                expand = True,
+                                text_align = ft.TextAlign.CENTER,
+                                color = "white",
+                                size = 50
+                            ),
+                            alignment=ft.alignment.center,
+                        )
+                    ]
                 ),
                 mouse_cursor = ft.MouseCursor.CLICK,
                 on_tap = do_something
