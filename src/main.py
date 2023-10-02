365c19f664c7632a3f579c5cc5bb2dae19ceca90
Sean Hong <hongsean1@gmail.com>
2023-10-01 23:21:04 -0400

    reformatted grid

--- src/main.py
+++ src/main.py
@@ -9,12 +9,22 @@ def main(page: ft.Page):
     player_symbol = 'X'
     computer_symbol = 'O'
 
+    # scale grid accordingly
+    def update_grid_sizes(_):
+        grid_board.width = page.window_width
+        grid_board.height = page.window_height
+        grid_board.child_aspect_ratio = grid_board.width / grid_board.height
+
+        page.update()
+
+    page.on_resize = update_grid_sizes
+
     # click event
     def run_game_logic(e):
         nonlocal game_over
 
         if not game_over:
-            e.control.content.controls[1].content.value = player_symbol
+            e.control.content.content.value = player_symbol
             display_symbols[e.control.key] = player_symbol
 
             # update page
@@ -26,7 +36,7 @@ def main(page: ft.Page):
                 return
 
             computer_action = computer_move(display_symbols)
-            grid_board.controls[computer_action].content.controls[1].content.value = computer_symbol
+            grid_board.controls[computer_action].content.content.value = computer_symbol
             display_symbols[computer_action] = computer_symbol
 
             # update page
@@ -38,21 +48,25 @@ def main(page: ft.Page):
                 return
 
     # create the grid board
-    grid_board = ft.GridView(expand=True, runs_count=3)
+    grid_board = ft.GridView(
+        width = page.window_width,
+        height = page.window_height,
+        expand = True,
+        runs_count = 3,
+        child_aspect_ratio = (page.window_width / page.window_height)
+    )
 
     # fill each grid cell with a click handler
     for i in range(0, 9):
         grid_board.controls.append(
             ft.GestureDetector(
                 key = i,
-                content = ft.Stack([
-                    ft.Image("square.png", expand=True),
-                    ft.Container(
-                        content = ft.Text('', expand=True, text_align=ft.TextAlign.CENTER, size=50, color="white"),
-                        expand = True,
-                        alignment = ft.alignment.center
-                    )
-                ]),
+                content = ft.Container(
+                    content = ft.Text('', expand=True, text_align=ft.TextAlign.CENTER, size=50, color="white"),
+                    expand = True,
+                    alignment = ft.alignment.center,
+                    bgcolor = "red"
+                ),
                 mouse_cursor = ft.MouseCursor.CLICK,
                 on_tap = run_game_logic
             )
